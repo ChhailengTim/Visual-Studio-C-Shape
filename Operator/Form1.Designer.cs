2006c32ab0a5cce36ef5ac01661542f964daf6ac
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2021-10-27 22:58:35 +0700

    Day 3 Operator

---
+++ Operator/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Operator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textStart = new System.Windows.Forms.TextBox();
+            this.textStop = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(310, 79);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time Start";
+            // 
+            // textStart
+            // 
+            this.textStart.Location = new System.Drawing.Point(163, 84);
+            this.textStart.Name = "textStart";
+            this.textStart.Size = new System.Drawing.Size(100, 20);
+            this.textStart.TabIndex = 2;
+            // 
+            // textStop
+            // 
+            this.textStop.Location = new System.Drawing.Point(163, 130);
+            this.textStop.Name = "textStop";
+            this.textStop.Size = new System.Drawing.Size(100, 20);
+            this.textStop.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Time Stop";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(310, 125);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(143, 174);
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(257, 20);
+            this.textResult.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result-Used";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 328);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.textStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textStart;
+        private System.Windows.Forms.TextBox textStop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Operator/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Operator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //int i = 1234;
+            //double d = i;
+            //double d = 12342154365215;
+            //int i = (int)d; //inplicit
+            //int i = Convert.ToInt32(d); //explicit
+            //MessageBox.Show(d.ToString());
+
+            //DateTime dt = DateTime.Parse("10/27/2021"); //explicit
+
+            //DateTime dt;
+            //bool b = DateTime.TryParse("10/27/2021",out dt); //implicit
+            //if (b==true)
+            //{
+            //    MessageBox.Show(dt.ToShortDateString());
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Date is wrong");
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textStart.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            textStop.Text = DateTime.Now.ToLongTimeString();
+
+            string Start = textStart.Text;
+            string Stop = textStop.Text;
+
+            TimeSpan Result = DateTime.Parse(Stop) - DateTime.Parse(Start);
+
+            textResult.Text = Start + " and " + Stop + " duration " + Result.ToString();
+
+        }
+    }
+}
