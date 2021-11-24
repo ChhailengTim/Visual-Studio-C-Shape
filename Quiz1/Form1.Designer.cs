ed06afe805d00e82e0b3e5c9b4e70086708bb842
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2021-11-24 20:29:54 +0700

    Day 7

---
+++ Quiz1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace Quiz1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAdd = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bid";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Qty";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Author";
+            // 
+            // txtAdd
+            // 
+            this.txtAdd.Location = new System.Drawing.Point(127, 86);
+            this.txtAdd.Name = "txtAdd";
+            this.txtAdd.Size = new System.Drawing.Size(153, 23);
+            this.txtAdd.TabIndex = 4;
+            this.txtAdd.TextChanged += new System.EventHandler(this.txtAdd_TextChanged);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(127, 123);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(153, 23);
+            this.txtTitle.TabIndex = 5;
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(127, 159);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(153, 23);
+            this.txtQty.TabIndex = 6;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(127, 195);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(153, 23);
+            this.txtAuthor.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(340, 81);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(340, 123);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 9;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(340, 159);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(35, 233);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(334, 184);
+            this.listBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAdd;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Quiz1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAdd_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtAdd.Text != "")
+            {
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    string[] st = listBox1.Items[i].ToString().Split('\t');
+                    if (st[0] == txtAdd.Text)
+                    {
+                        txtAdd.Text = st[0];
+                        txtTitle.Text = st[1];
+                        txtQty.Text = st[2];
+                        txtAuthor.Text = st[3];
+                        listBox1.SelectedIndex = i;
+                        return;
+                    }
+
+                }
+                MessageBox.Show("Search book not found");
+            }
+            else MessageBox.Show("Please Enter book id to search");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                if (MessageBox.Show("Are you sure delete this item", "", MessageBoxButtons.YesNo) ==
+                    DialogResult.Yes)
+                {
+                    for (int i = listBox1.SelectedItems.Count; i > 0; i--)
+                    {
+                        listBox1.Items.Remove(listBox1.SelectedItem);
+                    }
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtAdd.Text != "" && txtQty.Text != "" && txtTitle.Text != "" && txtAuthor.Text != "")
+            {
+
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    string[] st = listBox1.Items[i].ToString().Split('\t');
+                    if (st[0] == txtAdd.Text)
+                    {
+                        if (MessageBox.Show("Update information", "", MessageBoxButtons.YesNo) ==
+                            DialogResult.Yes)
+                        {
+                            listBox1.Items[i] = txtAdd.Text + "\t" + txtTitle.Text + "\t" +
+                                   txtQty.Text + "\t" + txtAuthor.Text;
+                            txtAdd.Clear(); txtTitle.Clear(); txtQty.Clear(); txtAuthor.Clear();
+                            txtAdd.Focus();
+                        }
+                        return;
+                    }
+                }
+                listBox1.Items.Add(txtAdd.Text + "\t" + txtTitle.Text + "\t" +
+                                   txtQty.Text + "\t" + txtAuthor.Text);
+                txtAdd.Clear(); txtTitle.Clear(); txtQty.Clear(); txtAuthor.Clear();
+                txtAdd.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Please verify you TextBox input again!");
+            }
+
+        }
+    }
+
+}
