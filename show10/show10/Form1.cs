04cbd3a424a4ccf85c29f632bbaf8eda008d5598
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-05 20:05:57 +0700

    Teacher Original file

---
+++ show10/show10/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace show10
+{
+    class ParentClass {
+        public ParentClass() {
+            MessageBox.Show("Welcome Parent class show");
+        }
+        public ParentClass(string st) {
+            MessageBox.Show(st);
+        }
+    }
+    class ChildClass : ParentClass {
+        public ChildClass(): base("Hello! Parent class show") {
+            MessageBox.Show("Welcome Child class show");
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ChildClass obj = new ChildClass();
+
+            //student stu = new student();
+            //stu.name = "dara";
+            //stu.sex = "male";
+            //stu.address = "pp";
+            //stu.Class = "E7";
+            //stu.show_info();
+            //staff st = new staff();
+            //st.name = "Bora";
+            //st.sex = "female";
+            //st.address = "pp";
+            //st.salary = 600;
+            //st.show_info();
+        }
+    }
+    class sample { 
+    }
+    class Person {
+        public string name;
+        public string sex;
+        public string address;
+
+        public void show_info() {
+            MessageBox.Show("Person name: " + name + " Sex: " + sex + " address: " + address);
+        }
+    }
+    class student : Person {
+        public string Class;
+    }
+    class staff : Person {
+        public double salary;
+    }
+}
---
+++ show11/show11/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace show11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Iperson obj = new person();
+            ((Iperson1)obj).show();
+            ((Iperson2)obj).show();
+            //obj.show2();
+        }
+    }
+    interface Iperson1 {
+        void show(); //{ MessageBox.Show("Welcome Iperson show"); }
+    }
+    interface Iperson2 {
+        void show();
+    }
+    interface Iperson : Iperson1, Iperson2 { }
+    //class sample : Form1, person { }
+    class person:Iperson {
+        void Iperson1.show() { MessageBox.Show("Welcome Iperson1 show"); }
+        void Iperson2.show() { MessageBox.Show("Welcome Iperson2 show"); }
+        //public void show1() { MessageBox.Show("Welcome Iperson1 show"); }
+        //public void show2() { MessageBox.Show("welcome Iperson2 show"); }
+    }
+}
