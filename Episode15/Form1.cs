09f5d4ae58d42db05c8d057d967f4617c72fdb8f
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-21 18:36:47 +0700

    Delegate Method

--- Episode15/Form1.cs
+++ Episode15/Form1.cs
@@ -23,20 +23,42 @@ namespace Episode15
             //del_show del_obj = new del_show(show);
             //del_obj("Welcome delegate function");
             //sample2
-            List<student> obj = new List<student>();
-            obj.Add(new student("nora", "male", 95));
-            obj.Add(new student("lola", "male", 40));
-            obj.Add(new student("koko", "female", 45));
-            obj.Add(new student("momo", "male", 90));
-            obj.Add(new student("naro", "female", 85));
+            //List<student> obj = new List<student>();
+            //obj.Add(new student("nora", "male", 95));
+            //obj.Add(new student("lola", "male", 40));
+            //obj.Add(new student("koko", "female", 45));
+            //obj.Add(new student("momo", "male", 90));
+            //obj.Add(new student("naro", "female", 85));
+            //delpass_student dp = new delpass_student(ispass_student);
 
+            //student.pass_student(obj,dp);
 
-            student.pass_student(obj);
+            //student.pass_student(obj, x=>x.average>=50);
+
+            //sample3
+            delshow del1, del2, del3, del;
+            del1 = new delshow(show1);
+            del2 = new delshow(show2);
+            del3 = new delshow(show3);
+            del = del1 + del2 + del3;
+            del();
+
+        }
+        public delegate void delshow();
+        public void show1() { MessageBox.Show("Welcome show1"); }
+        public void show2() { MessageBox.Show("Welcome show2"); }
+        public void show3() { MessageBox.Show("Welcome show3"); }
+        public bool ispass_student(student s)
+        {
+            //statement;
+            if (s.average >= 50) return true;
+            else return false;
         }
         //public void show(string st)
         //{
         //    MessageBox.Show(st);
         //}
+        public delegate bool delpass_student(student st);
         public class student
         {
             public string name { set; get; }
@@ -46,11 +68,11 @@ namespace Episode15
             {
                 name = n;sex = s;average = a;
             }
-            public static void pass_student(List<student> stu)
+            public static void pass_student(List<student> stu,delpass_student dp)
             {
                 foreach(student s in stu)
                 {
-                    if (s.average >= 50)
+                    if (dp(s))
                     {
                         MessageBox.Show("Student name: " + s.name + " Sex: " + s.sex + " Average: " + s.average);
                     }
