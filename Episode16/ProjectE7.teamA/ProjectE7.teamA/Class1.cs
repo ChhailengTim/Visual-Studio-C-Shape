d44d9c10f478b2f5b5adc0c250523a0741ac905f
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-25 21:07:54 +0700

    create class library teamA

---
+++ Episode16/ProjectE7.teamA/ProjectE7.teamA/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectE7
+{
+    namespace teamA
+    {
+        public class ClassA
+        {
+            public static string show()
+            {
+                return "Welcome class A team A";
+            }
+        }
+    }
+}
