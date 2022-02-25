6167516ce57e428da2dcd3cfbd32f26685b4cfa1
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-25 21:07:18 +0700

    Difference Struct and Class

--- Episode16/Form1.cs
+++ Episode16/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using PA = ProjectE7.teamA;
+using PB = ProjectE7.teamB;
 
 namespace Episode16
 {
@@ -17,5 +12,34 @@ namespace Episode16
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //int i = 10;
+            //int j = i;
+            //i = 20;
+            //MessageBox.Show("Value i=" + i + " and j=" + j);
+            //person p = new person("bora","male",20);
+            //person p1 = p;
+            //p.name = "naro";
+            //MessageBox.Show("P name=" + p.name + " sex=" + p.sex + " age=" + p.age + "\n"+
+            //    "P1 name= "+p1.name+" sex= "+p1.sex+" age="+p1.age);
+            MessageBox.Show(PA.ClassA.show());
+            MessageBox.Show(PB.ClassA.show());
+        }
+    }
+    struct person
+    {
+        public string name;
+        public string sex;
+        public int age;
+        public person(string n,string s,int a)
+        {
+            name = n;sex = s;age = a;
+        }
+        //public person() { }
+        //~person()
+        //{
+        //    MessageBox.Show("Bye bye class person");
+        //}
     }
 }
