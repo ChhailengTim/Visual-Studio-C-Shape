641a2c4d50c456449fc52091f6a50d85eb038c05
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-09 20:18:55 +0700

    Update Form1.cs

--- Episode18/Form1.cs
+++ Episode18/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Episode18
 {
@@ -15,6 +16,55 @@ namespace Episode18
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+            //FileStream Fs = new FileStream("Episode18.txt", FileMode.Append);
+            //StreamWriter sw = new StreamWriter(Fs);
+            //sw.WriteLine("nara@male@0123456789");
+            //sw.Close();
+            //Fs.Close();
+
+            //FileStream Fs = new FileStream("Episode18.txt", FileMode.Open);
+            //StreamReader sr = new StreamReader(Fs);
+
+            //while (!sr.EndOfStream)
+            //{
+            //    MessageBox.Show(sr.ReadLine());
+            //}
+            //Fs.Close();
+        }
+
+        private void textRoom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FileStream Fs = new FileStream("holtel.txt", FileMode.Append);
+            StreamWriter sw = new StreamWriter(Fs);
+            sw.WriteLine( textRoom.Text +"/" + textCustName.Text +"/"+ textDate.Text);
+            //sw.WriteLine("Customer Name :" + textCustName.Text);
+            //sw.WriteLine("Date :" + textDate.Text);
+            sw.Close();
+            Fs.Close();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            FileStream Fs = new FileStream("holtel.txt", FileMode.Open);
+           StreamReader sr = new StreamReader(Fs);
+            MessageBox.Show(sr.ReadLine());
+            string st = "";
+            while (!sr.EndOfStream)
+            {
+                st += sr.ReadLine() + "\n";
+            }
+            MessageBox.Show(st);
+            Fs.Close();
         }
     }
 }
