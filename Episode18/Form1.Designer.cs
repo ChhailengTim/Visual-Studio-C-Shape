0aa44486463128cc58db99fb4ad8c200d6faf483
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-09 20:18:53 +0700

    Update Form1.Designer.cs

--- Episode18/Form1.Designer.cs
+++ Episode18/Form1.Designer.cs
@@ -29,13 +29,116 @@ namespace Episode18
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textRoom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textCustName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textDate = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textRoom
+            // 
+            this.textRoom.Location = new System.Drawing.Point(162, 126);
+            this.textRoom.Name = "textRoom";
+            this.textRoom.Size = new System.Drawing.Size(139, 20);
+            this.textRoom.TabIndex = 0;
+            this.textRoom.TextChanged += new System.EventHandler(this.textRoom_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Room";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(340, 124);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(340, 173);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 5;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cust_Name";
+            // 
+            // textCustName
+            // 
+            this.textCustName.Location = new System.Drawing.Point(162, 175);
+            this.textCustName.Name = "textCustName";
+            this.textCustName.Size = new System.Drawing.Size(139, 20);
+            this.textCustName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Date";
+            // 
+            // textDate
+            // 
+            this.textDate.Location = new System.Drawing.Point(162, 219);
+            this.textDate.Name = "textDate";
+            this.textDate.Size = new System.Drawing.Size(139, 20);
+            this.textDate.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(491, 455);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textDate);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textCustName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textRoom);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textRoom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textCustName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textDate;
     }
 }
 
