82b20d6de51089bf1f069bf98d37c86eeace7406
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-17 19:44:29 +0700

    Update Form1.cs

--- Episode18/Form1.cs
+++ Episode18/Form1.cs
@@ -8,11 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Episode18
 {
     public partial class Form1 : Form
     {
+        BinaryFormatter BF = new BinaryFormatter();
         public Form1()
         {
             InitializeComponent();
@@ -44,27 +46,73 @@ namespace Episode18
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            FileStream Fs = new FileStream("holtel.txt", FileMode.Append);
-            StreamWriter sw = new StreamWriter(Fs);
-            sw.WriteLine( textRoom.Text +"/" + textCustName.Text +"/"+ textDate.Text);
+            //FileStream Fs = new FileStream("holtel.txt", FileMode.Append);
+            //StreamWriter sw = new StreamWriter(Fs);
+            //sw.WriteLine( textRoom.Text +"/" + textCustName.Text +"/"+ textDate.Text);
             //sw.WriteLine("Customer Name :" + textCustName.Text);
             //sw.WriteLine("Date :" + textDate.Text);
-            sw.Close();
+            //sw.Close();
+            //Fs.Close();
+
+            FileStream Fs = new FileStream("HomeWork.txt", FileMode.Append);
+            RoomID obj = new RoomID(textRoom.Text);
+            BF.Serialize(Fs, obj);
+            CusName obj1 = new CusName(textCustName.Text);
+            BF.Serialize(Fs,obj1);
+            Days obj2 = new Days(textDate.Text);
+            BF.Serialize(Fs, obj2);
             Fs.Close();
+
+
+        }
+        [Serializable]
+        class RoomID
+        {
+            public string id { set; get; }
+            public RoomID(string roomid) { id = roomid;  }
+        }
+        [Serializable]
+        class CusName
+        {
+            public string name { set; get; }
+            public CusName(string cusname) { name = cusname; }
+        }
+        [Serializable]
+        class Days
+        {
+            public string day { set; get; }
+            public Days(string date) { day = date; }
         }
 
         private void btnShow_Click(object sender, EventArgs e)
         {
-            FileStream Fs = new FileStream("holtel.txt", FileMode.Open);
-           StreamReader sr = new StreamReader(Fs);
-            MessageBox.Show(sr.ReadLine());
-            string st = "";
-            while (!sr.EndOfStream)
-            {
-                st += sr.ReadLine() + "\n";
+            FileStream Fs = new FileStream("HomeWork.txt", FileMode.Open);
+            RoomID obj = new RoomID(textRoom.Text);
+            CusName obj1 = new CusName(textCustName.Text);
+            Days obj2 = new Days(textDate.Text);
+
+            while (Fs.Position != Fs.Length)
+                {
+                obj = (RoomID)BF.Deserialize(Fs);
+                obj1 = (CusName)BF.Deserialize(Fs);
+                obj2 = (Days)BF.Deserialize(Fs);
+                MessageBox.Show(obj.id+"\t"+obj1.name+"\t"+obj2.day);
             }
-            MessageBox.Show(st);
             Fs.Close();
         }
+
+        //    private void btnShow_Click(object sender, EventArgs e)
+        //    {
+        //        FileStream Fs = new FileStream("holtel.txt", FileMode.Open);
+        //       StreamReader sr = new StreamReader(Fs);
+        //        MessageBox.Show(sr.ReadLine());
+        //        string st = "";
+        //        while (!sr.EndOfStream)
+        //        {
+        //            st += sr.ReadLine() + "\n";
+        //        }
+        //        MessageBox.Show(st);
+        //        Fs.Close();
+        //    }
     }
 }
