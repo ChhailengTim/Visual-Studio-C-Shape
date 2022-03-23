63b5c0d28ed5659759668947965cac1ce4ba20e7
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-23 21:32:06 +0700

    my teacher code that I can't add report viewer to form design

---
+++ Episode20-data-report/teacher-code/show20/show20/ClsDataDesign.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace show20
+{
+    class ClsDataDesign
+    {
+        public string Pro_id { set; get; }
+        public string Pro_name { set; get; }
+        public int Qty { set; get; }
+        public double Price { set; get; }
+        public ClsDataDesign(string id, string name, int q, double p) {
+            Pro_id = id; Pro_name = name; Qty = q; Price = p;
+        }
+    }
+}
---
+++ Episode20-data-report/teacher-code/show20/show20/ClsSource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace show20
+{
+    class ClsSource: List<ClsDataDesign>
+    {
+        public ClsSource() {
+            Add(new ClsDataDesign("p001", "cocacola", 100, 8.5));
+            Add(new ClsDataDesign("p002", "7-up", 100, 7.5));
+            Add(new ClsDataDesign("p003", "tiger", 100, 22.5));
+            Add(new ClsDataDesign("p004", "anchor", 100, 15.5));
+        }
+    }
+}
