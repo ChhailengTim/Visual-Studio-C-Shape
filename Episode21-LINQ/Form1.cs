34ef13f4d9a25c378ff016e6b61eb9a8d47adf2a
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-30 20:17:34 +0700

    objects

--- Episode21-LINQ/Form1.cs
+++ Episode21-LINQ/Form1.cs
@@ -33,22 +33,63 @@ namespace Episode21_LINQ
             //    }
 
 
-            string[] host_name = { "asp.net", "kkbc.org", "dara.net", "bopha.org", "aming.kh", "tola.com", "kkbbc.com", "nono.net", "lola.", "momo.tv" };
+            //string[] host_name = { "asp.net", "kkbc.org", "dara.net", "bopha.org", "aming.kh", "tola.com", "kkbbc.com", "nono.net", "lola.", "momo.tv" };
 
-            var source = from v in host_name
-                         where v.Substring(v.LastIndexOf(".")).Length > 3
-                         //orderby v descending //big to small
-                         orderby v ascending //small to big
-                         select v;
+            ////MessageBox.Show(host_name[0].Substring(0));
+            //var source = from v in host_name
+            //             where v.Substring(v.LastIndexOf(".")).Length > 3
+            //             //orderby v descending //big to small
+            //             orderby v ascending //small to big
+            //             select v;
 
 
+            //string k = "";
+            //foreach(string s in source)
+            //{
+            //    k += s + "\n";
+            //}
+            //MessageBox.Show(k);
+
+            product[] obj = new product[]
+            { 
+                new product("cocacola", 100, 8.5),
+                new product("cocacola", 100, 8.5),
+                new product("hanuman", 50, 8.6),
+                new product("tiger", 80, 25.5),
+                new product("abc", 150, 25.5),
+                new product("anchor", 200, 15.5),
+
+            };
+
+            var source = from v in obj
+                         where v.price<20
+                         orderby v.pname
+                             //select v;
+                         select new Lpro(v.pname, v.price);
             string k = "";
-            foreach(string s in source)
+            //foreach (product s in source)
+            //{
+            //    k += s.pname + "\t" + s.qty + "\t" + s.price + "\n";
+            //}
+            foreach(Lpro s in source)
             {
-                k += s + "\n";
+                k += s.pname + "\t" + s.price + "\n";
             }
             MessageBox.Show(k);
 
         }
+    }
+    class Lpro
+    {
+        public string pname { set; get; }
+        public double price { set; get; }
+        public Lpro(string n,double p) { pname = n; price = p; }
+    }
+    class product
+    {
+        public string pname { set; get; }
+        public int qty { set; get; }
+        public double price { set; get; }
+        public product(string n,int q,double p) { pname = n;qty = q;price = p; }
     }
 }
