40ba91e5a60953d271b79303d3970d991eee6fdf
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-11 19:54:31 +0700

    Update Form1.cs

--- Episode23-using-undataboun/Form1.cs
+++ Episode23-using-undataboun/Form1.cs
@@ -24,13 +24,23 @@ namespace Episode23_using_undataboun
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            cnn.ConnectionString = "Data Source=NAME;Intial catalog=TABLE-NAME;Integrated Security=True";
+            cnn.ConnectionString = "Data Source=NAME;Intial catalog=DATABASE-NAME;Integrated Security=True";
             cnn.Open();
             cmm.Connection = cnn;
 
             //SQL statements; (Execute: Insert,Delete,Update,Select:)
-            cmm.CommandText = "Insert into TABLE_NAME values('data','data','data','data')";
-            cmm.ExecuteNonQuery();
+                                //Delete From TABLE where condition
+                                //Update TABLE set f1=v,f2=v,... where condition
+            //cmm.CommandText = "Insert into TABLE_NAME values('data','data','data','data')";
+            //cmm.ExecuteNonQuery();
+
+            cmm.CommandText = "Select * from TABLE_NAME";
+            dr = cmm.ExecuteReader();
+            while (dr.Read())
+            {
+                MessageBox.Show(dr[0] + "\t" + dr[1] + "\t" + dr[2] + "\t" + dr[3] + "\t" + dr[4]);
+            }
+            dr.Close();
         }
     }
 }
