729ade7c209f71c6f96697f49a817bf2561e36af
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-18 19:27:28 +0700

    Sql command

--- Episode24-access-database-command/Form1.cs
+++ Episode24-access-database-command/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Episode24_access_database_command
 {
     public partial class Form1 : Form
     {
+        SqlConnection cnn = new SqlConnection();
+        SqlCommand cmm = new SqlCommand();
+
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        DataView dv = new DataView();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
