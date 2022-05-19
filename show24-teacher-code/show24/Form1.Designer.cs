b8ad397bddb71db386f5d3cdaf893dc4c035f4b4
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-19 07:52:44 +0700

    teacher code how to access database via command

---
+++ show24-teacher-code/show24/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace show24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtBid = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBtitle = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDates = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(68, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(760, 257);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtBid
+            // 
+            this.txtBid.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBid.Location = new System.Drawing.Point(342, 359);
+            this.txtBid.Name = "txtBid";
+            this.txtBid.Size = new System.Drawing.Size(361, 41);
+            this.txtBid.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 362);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "BID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(172, 409);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 36);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "B_title";
+            // 
+            // txtBtitle
+            // 
+            this.txtBtitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBtitle.Location = new System.Drawing.Point(342, 406);
+            this.txtBtitle.Name = "txtBtitle";
+            this.txtBtitle.Size = new System.Drawing.Size(361, 41);
+            this.txtBtitle.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(172, 456);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 36);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Qty";
+            // 
+            // txtQty
+            // 
+            this.txtQty.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQty.Location = new System.Drawing.Point(342, 453);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(361, 41);
+            this.txtQty.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(172, 503);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 36);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Author";
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAuthor.Location = new System.Drawing.Point(342, 500);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(361, 41);
+            this.txtAuthor.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(172, 550);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 36);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Dates";
+            // 
+            // txtDates
+            // 
+            this.txtDates.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDates.Location = new System.Drawing.Point(342, 547);
+            this.txtDates.Name = "txtDates";
+            this.txtDates.Size = new System.Drawing.Size(361, 41);
+            this.txtDates.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 649);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDates);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBtitle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBid);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtBid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBtitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDates;
+    }
+}
+
