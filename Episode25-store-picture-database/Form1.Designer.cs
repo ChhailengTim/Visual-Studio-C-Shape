0d1f8c31ac10959ac93a0eff471f93a74e91cf17
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-01 20:15:15 +0700

    how to save picture to database with byte type

---
+++ Episode25-store-picture-database/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace Episode25_store_picture_database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPreious = new System.Windows.Forms.Button();
+            this.txtSex = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "St_Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(120, 87);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(172, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(353, 85);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(353, 126);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(120, 128);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(172, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name";
+            // 
+            // btnPreious
+            // 
+            this.btnPreious.Location = new System.Drawing.Point(353, 172);
+            this.btnPreious.Name = "btnPreious";
+            this.btnPreious.Size = new System.Drawing.Size(75, 23);
+            this.btnPreious.TabIndex = 8;
+            this.btnPreious.Text = "Previous";
+            this.btnPreious.UseVisualStyleBackColor = true;
+            // 
+            // txtSex
+            // 
+            this.txtSex.Location = new System.Drawing.Point(120, 174);
+            this.txtSex.Name = "txtSex";
+            this.txtSex.Size = new System.Drawing.Size(172, 20);
+            this.txtSex.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sex";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(353, 219);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(120, 221);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(172, 20);
+            this.txtPhone.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Phone";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(353, 279);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 12;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(120, 252);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 207);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 279);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Photos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 528);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnPreious);
+            this.Controls.Add(this.txtSex);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPreious;
+        private System.Windows.Forms.TextBox txtSex;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Episode25-store-picture-database/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Episode25_store_picture_database
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cnn = new SqlConnection();
+        SqlCommand cmm = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        SqlDataReader dr; // = new SqlDataReader();
+        DataTable dt = new DataTable();
+        DataRow drow;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cnn.ConnectionString = "data source=DEVICE;initial catalog=DATABASENAME;integrate security=true";
+            cnn.Open();
+            cmm.Connection = cnn;
+
+            cmm.CommandText = "Select * from DATABASENAME";
+            dr = cmm.ExecuteReader();
+            dr.Read();
+            txtId.Text = dr[0].ToString();
+            txtName.Text = dr[1].ToString();
+            txtSex.Text = dr[2].ToString();
+            txtPhone.Text = dr[3].ToString();
+
+            MemoryStream ms = new MemoryStream((byte[])dr[4]);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = Image.FromStream(ms);
+            dr.Close();
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            cmm.CommandText = "select * from tblstaff";
+            da = new SqlDataAdapter(cmm);
+            dt = new DataTable();
+            da.Fill(dt);
+            drow = dt.NewRow();
+            drow[0] = txtId.Text;
+            drow[1] = txtName.Text;
+            drow[2] = txtSex.Text;
+            drow[3] = txtPhone.Text;
+            MemoryStream ms = new MemoryStream();
+            pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
+            drow[4] = ms.GetBuffer();
+            dt.Rows.Add(drow);
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+            da.Update(dt);
+            txtId.Clear(); txtName.Clear(); txtSex.Clear(); txtPhone.Clear();
+            pictureBox1.Image = null; txtId.Focus();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog.ShowDialog();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            //pictureBox1.Image = System.Drawing.Image.FromFile(OpenFileDialog.FileName);
+        
+    }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
